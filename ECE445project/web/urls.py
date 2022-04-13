1ae95f203911c36446562a99b8ac189dcfda3cf5
Xiao Shuhong <Shuhong.18@intl.zju.edu.cn>
2022-04-13 09:22:26 +0800

    4.13

--- ECE445project/web/urls.py
+++ ECE445project/web/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('upload_clothes/index',views.upload_clothes_index,name='upload_clothes_index'),
+    path('upload_clothes/upload',views.do_upload,name='upload_clothes'),
+    path('gallery/index',views.gallery_index,name='gallery_index'),
     
 ]
--- ECE445project/web/views.py
+++ ECE445project/web/views.py
@@ -1,15 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+
 # Create your views here.
 def index(request):
     # return HttpResponse("hello world")
     return render(request,"Home.html")
 
-def back_home(request):
+def upload_clothes_index(request):
+    return render(request,"upload_clothes.html")
+
+def do_upload(request):
+    try:
+        file=request.POST.get("filename",None)
+        file_content=request.FILES.get("file_content")
+        # print(file_content)
+        if not file_content:
+             return render(request,"upload_fail.html")
+        destination = open("./static/user_upload/"+file+".jpg","wb+")
+        for chunk in file_content.chunks():     
+            destination.write(chunk)  
+        destination.close()
+       
+    except:
+        print("error")
+        
+    return render(request,"upload_success.html")
     
-    return render(request,"Home.html")
 
 
+def gallery_index(request):
+    return render(request,"gallery_body.html")
+
+    
+    
+# def upload_success(request):
+#     return render(request,'update_success.html')
 
 
 
